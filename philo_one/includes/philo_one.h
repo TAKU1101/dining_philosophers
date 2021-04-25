343f4468b5b6677361cea968913b2d1a763d7bc0
TAKU1101 <taku.kuma1101@icloud.com>
2021-04-25 22:24:36 +0900

    Add: one init

---
+++ philo_one/includes/philo_one.h
@@ -0,0 +1,8 @@
+#ifndef PHILO_ONE
+# define PHILO_ONE
+
+# include <stdio.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
---
+++ philo_one/srcs/main.c
@@ -0,0 +1,13 @@
+#include "philo_one.h"
+
+int	init_philos()
+{
+	return (0);
+}
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	return (0);
+}
