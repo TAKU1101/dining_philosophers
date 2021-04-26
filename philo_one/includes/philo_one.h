c358f0f37241849bdd2b83145445a5398a153909
TAKU1101 <taku.kuma1101@icloud.com>
2021-04-26 19:55:14 +0900

    Add: philo init

--- philo_one/includes/philo_one.h
+++ philo_one/includes/philo_one.h
@@ -5,4 +5,31 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+typedef struct s_philo
+{
+	pthread_t		thread;
+	long			last_eat_time;
+}					t_philo;
+
+typedef struct s_monitor
+{
+	pthread_mutex_t	monitor;
+}					t_monitor;
+
+typedef struct s_info
+{
+	int				num_of_people;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				must_eat;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+	t_monitor		monitor;
+}					t_info;
+
+int					ft_atoi(const char *str);
+int					ft_strncmp(const char *str1, const char *str2, size_t n);
+int					ft_isdigit(int c);
+
 #endif
--- philo_one/srcs/main.c
+++ philo_one/srcs/main.c
@@ -1,12 +1,41 @@
 #include "philo_one.h"
 
-int	init_philos()
+int	init_philos(int num)
 {
+	int i;
+
+	i = 0;
+	while (i < num)
+	{
+		i++;
+	}
+	return (0);
+}
+
+int	init_info(t_info *info, int argc, char *argv[])
+{
+	if (argc != 5 && argc != 6)
+		return (1);
+	info->num_of_people = ft_atoi(argv[1]);
+	info->time_to_die = ft_atoi(argv[2]);
+	info->time_to_eat = ft_atoi(argv[3]);
+	info->time_to_sleep = ft_atoi(argv[4]);
+	info->must_eat = -1;
+	if (argc == 6)
+	{
+		info->must_eat = ft_atoi(argv[5]);
+		if (info->must_eat < 0)
+			return (1);
+	}
 	return (0);
 }
 
 int main(int argc, char *argv[])
 {
+	t_info	info;
+
+	if (init_info(&info, argc, argv))
+		return (1);
 	(void)argc;
 	(void)argv;
 	return (0);
