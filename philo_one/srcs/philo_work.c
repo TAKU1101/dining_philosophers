d039687aa8a4348f0544288e85b97ccbdc33a204
TAKU1101 <taku.kuma1101@icloud.com>
2021-05-08 12:38:18 +0900

    Add: is_dead

--- philo_one/srcs/philo_work.c
+++ philo_one/srcs/philo_work.c
@@ -15,16 +15,42 @@ static int	philo_take_fork(t_philo *philo)
 	return (0);
 }
 
+static int	is_dead(t_philo *philo)
+{
+	long	now;
+	int		ret;
+
+	now = get_time();
+	ret = 0;
+	if (pthread_mutex_lock(&philo->let_mutex))
+		return (error_log(ERROR_MUTEX_LOCK));
+	if ((now - philo->last_eat_time) >= philo->time_to_die)
+		ret = 1;
+	if (pthread_mutex_unlock(&philo->let_mutex))
+		return (error_log(ERROR_MUTEX_UNLOCK));
+	return (ret);
+}
+
 static int	philo_after_eat(t_philo *philo)
 {
 	philo_log(philo->philo_nb, LOG_EATING);
+	philo->eat_nb = philo->eat_nb + 1;
+	if (pthread_mutex_lock(&philo->let_mutex))
+		return (error_log(ERROR_MUTEX_LOCK));
+	philo->last_eat_time = get_time();
+	if (pthread_mutex_unlock(&philo->let_mutex))
+		return (error_log(ERROR_MUTEX_UNLOCK));
 	usleep(philo->time_to_eat * 1000);
 	if (pthread_mutex_unlock(philo->left))
 		return (error_log(ERROR_MUTEX_UNLOCK));
 	if (pthread_mutex_unlock(philo->right))
 		return (error_log(ERROR_MUTEX_UNLOCK));
+	if (is_dead(philo))
+		return (1);
 	philo_log(philo->philo_nb, LOG_SLEEPING);
 	usleep(philo->time_to_sleep * 1000);
+	if (is_dead(philo))
+		return (1);
 	philo_log(philo->philo_nb, LOG_THINKING);
 	return (0);
 }
@@ -32,14 +58,16 @@ static int	philo_after_eat(t_philo *philo)
 void	*philo_work(void *arg)
 {
 	t_philo	*philo;
+	int ret;
 
 	philo = (t_philo *)arg;
 	if (philo->philo_nb % 2)
 		usleep(200);
-	while (1)
+	ret = 0;
+	while (!ret)
 	{
 		philo_take_fork(philo);
-		philo_after_eat(philo);
+		ret = philo_after_eat(philo);
 	}
 	return (NULL);
 }
