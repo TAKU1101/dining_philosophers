597f8ba799af6ad14727230a5f0f83baf50c2e4e
TAKU1101 <taku.kuma1101@icloud.com>
2021-05-12 00:17:58 +0900

    Add: philo_two init

---
+++ philo_two/includes/philo_two.h
@@ -0,0 +1,6 @@
+#ifndef PHILO_TWO_H
+# define PHILO_TWO_H
+
+# include <stdio.h>
+
+#endif
---
+++ philo_two/srcs/main.c
@@ -0,0 +1,9 @@
+#include "philo_two.h"
+
+int	main(int argc, char *argv[])
+{
+	printf("test\n");
+	return (0);
+	(void)argc;
+	(void)argv;
+}
