d822db71ccc6021f8f22ad61a94570188ea36845
TAKU1101 <taku.kuma1101@icloud.com>
2021-05-19 21:22:59 +0900

    fix: philo_one: dead log time

--- philo_one/srcs/monitor.c
+++ philo_one/srcs/monitor.c
@@ -4,14 +4,34 @@ void	*monitor(void *arg)
 {
 	t_info	*info;
 	int		i;
+	int		ret;
 
 	info = (t_info *)arg;
 	i = 0;
-	while (*(info->is_dead) != info->num_of_people)
+	if (pthread_mutex_lock(info->is_dead_mutex))
+		return (NULL);
+	ret = *(info->is_dead);
+	if (pthread_mutex_unlock(info->is_dead_mutex))
+		return (NULL);
+	while (ret != info->num_of_people)
 	{
+		if (pthread_mutex_lock(&(info->philos[i].let_mutex)))
+			return (NULL);
+		if (get_time() - info->philos[i].last_eat_time >= info->time_to_die)
+		{
+			philo_log(i + 1, LOG_DIED);
+			*(info->is_dead) = info->num_of_people;
+		}
+		if (pthread_mutex_unlock(&(info->philos[i].let_mutex)))
+			return (NULL);
 		i++;
 		if (i == info->num_of_people)
 			i = 0;
+		if (pthread_mutex_lock(info->is_dead_mutex))
+			return (NULL);
+		ret = *(info->is_dead);
+		if (pthread_mutex_unlock(info->is_dead_mutex))
+			return (NULL);
 	}
 	return (NULL);
 }
