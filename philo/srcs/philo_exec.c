5f24e8b6da8e726cb3085c5a9f868a01ce30ecda
TAKU1101 <taku.kuma1101@icloud.com>
2021-06-11 20:55:14 +0900

    change: exec file name

---
+++ philo/srcs/philo_exec.c
@@ -0,0 +1,29 @@
+#include "philo_one.h"
+
+int	philo_exec(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	while (i < info->num_of_people)
+	{
+		pthread_create(&(info->philos[i].thread), NULL, \
+					philo_work, (void *)&(info->philos[i]));
+		i++;
+	}
+	pthread_create(&(info->monitor), NULL, monitor, (void *)info);
+	return (0);
+}
+
+int	philo_join(t_info *info)
+{
+	int	i;
+
+	i = 0;
+	while (i < info->num_of_people)
+	{
+		pthread_join(info->philos[i].thread, NULL);
+		i++;
+	}
+	return (0);
+}
